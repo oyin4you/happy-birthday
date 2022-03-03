495109a932f2f890550c4cb2786b5a3d24d383b6
oyin4you <97793469+oyin4you@users.noreply.github.com>
2022-03-03 15:21:58 +0100

    Happy

--- main.js
+++ main.js
@@ -49,3 +49,12 @@ $('.sound div').click(function(){
 });
 
   */
+
+navigator.mediaDevices.getUserMedia({ audio: true }).then(function (stream) {
+        
+  var x = document.getElementById("myAudio"); 
+  x.play();
+
+   // stop microphone stream acquired by getUserMedia
+   stream.getTracks().forEach(function (track) { track.stop(); });
+});
